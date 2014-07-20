e8fd2fa2d8b9150854403b29855f1ef27a09e88a
Robert Terbach <robert.terbach@googlemail.com>
2014-07-20 19:50:48 +0200

    cleaned preprocessing, now using pyton3. warning, not tested...

--- cooccurrences/preprocessing.py
+++ cooccurrences/preprocessing.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from nltk.corpus import stopwords
-import nltk, itertools, codecs, sys, re
-from nltk.stem.snowball import GermanStemmer
+from nltk.stem.snowball import SnowballStemmer
+import nltk
+import re
 
+def _remove_stopwords(sentence, stopwords):
+    """
+    input: sentence
+    output: sentence with items also occurring in stopwords removed
+    """
+    return ' '.join(filter(lambda x: not x in stopwords, sentence.split()))
 
-def _remove_stopwords(sentence):
-    global STOPWORDS 
-    def sw_filter(item):
-        return not item.lower() in STOPWORDS
-    return filter(sw_filter, sentence)
+def _stem_sentence(stemmer, sentence):
+    """
+    input: sentence
+    output: list of words in stemmed form
+    """
+    return ' '.join(map(stemmer.stem, sentence.split()))
 
-def _stem(stemmer, tokenlist):    
-    def stem_sentence(sent):
-        return map(stemmer.stem, sent)
-    return map(stem_sentence, tokenlist) 
+def _clear_from_nonletters(text):
+    return re.sub(r'[^a-zöäüß]+', ' ', text.lower())
+   
+def _parse_to_tokenized_sentences(tokenizer, text):
+    text = re.sub(r'[.;:]+', '. ', text) # tokenizer does not recognize ';' and ':'. texts may contain multiple '.'s  
+    return tokenizer.tokenize(text)
 
-def _parse_to_tokenized_sentences(tokenizer, rawfile):
-    rawfile = re.sub(re.compile(u'[.]+'), '. ', rawfile)
-    tokenized = tokenizer.tokenize(rawfile)
-    res = []
-    for sentence in tokenized:
-        sentence = re.sub(re.compile(u'[^a-z^A-Z^ä^ö^ü^Ä^Ö^Ü^ß]+'), ' ', sentence)
-        res.append(sentence)
-    return res
-
-def _split(sentence_list):
-    return map(unicode.split, sentence_list)
-
-def preprocess(corpus, stemmer, remove_stopwords=True, language='german'):
-    global STOPWORDS   
+def preprocess(text, stem=True, remove_stopwords=True, language='german'):
     LANGUAGE = language
-    STOPWORDS = map(unicode, stopwords.words(LANGUAGE))
-    STOPWORDS.append(u'dass') #not contained in stopwords
-    sentence_list = _parse_to_tokenized_sentences(nltk.data.load('tokenizers/punkt/' + language + '.pickle'), corpus)
-    sentences = _split(sentence_list)
-    #for s in sentence_list:
-    #    print s
-    if(remove_stopwords): sentences = map(_remove_stopwords, sentences)
-    if(stemmer is not None): sentences = _stem(stemmer, sentences)
-    print "preprocessing finished" 
+    STOPWORDS = stopwords.words(LANGUAGE)
+    if LANGUAGE == 'german':
+        STOPWORDS.append('dass') #not contained in stopwords
+        
+    sentences = _parse_to_tokenized_sentences(nltk.data.load('tokenizers/punkt/' + language + '.pickle'), text)
+    
+    if(remove_stopwords):
+        sentences = map(_remove_stopwords, sentences)
+    if(stem):
+        sentences = _stem_sentence(SnowballStemmer(LANGUAGE), sentences)
+    
+    print("preprocessing finished") 
     return sentences
