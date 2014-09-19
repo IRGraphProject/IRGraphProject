b3172a1196d36c60f4df1298ea4aefff16b13d87
Robert Terbach <robert.terbach@googlemail.com>
2014-09-19 14:44:36 +0200

    added python3 graph-tool & ipython3

--- graph_tool/bootstrap.sh
+++ graph_tool/bootstrap.sh
@@ -4,3 +4,5 @@ echo "deb-src http://downloads.skewed.de/apt/saucy saucy universe" >> /etc/apt/s
 #echo "deb-src http://ppa.launchpad.net/mapnik/boos/ubuntu saucy main" >> /etc/apt/sources.list
 apt-get update
 apt-get install -y --force-yes python-graph-tool
+apt-get install -y --force-yes python3-graph-tool
+apt-get install -y --force-yes ipython3
