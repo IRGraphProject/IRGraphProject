f2d568ab75513d5b8fde1448200cb4973f6930de
katsel <katsel@users.noreply.github.com>
2014-10-31 17:12:56 +0100

    added edge width to graph. also: format and minor changes in documentation

--- graph_tool/graph-tool-test.py
+++ graph_tool/graph-tool-test.py
@@ -4,17 +4,26 @@ from graph_tool.all import *
 from wordsgraph import WordsGraph
 import graph_parser
 
-# erzeuge aus der Datei test_data_NL einen WordGraph (siehe wordsgraph.py für weite dokumentation)
+# erzeuge aus der Datei test_data_NL einen WordGraph
+# (siehe wordsgraph.py für weitere Dokumentation)
+
 g = graph_parser.file_to_graph('test_data_NL')
 print("graph created")
 
-# graph_tool stellt eine möglichkeit zur Berechnung des Durchmessers eines Graphen zur Verfügung. Die Methode braucht dazu das graph_tool Graph objekt, welches in unserem WordsGraph-Objekt unter dem Feld graph erreichbar ist.
+# graph_tool stellt eine möglichkeit zur Berechnung des Durchmessers eines
+# Graphen zur Verfügung. Die Methode braucht dazu das graph_tool Graph objekt,
+# welches in unserem WordsGraph-Objekt unter dem Feld graph erreichbar ist.
+
 #print("pseudo diameter: %s" % str(graph_tool.topology.pseudo_diameter(g.graph)))
 #print("global clustering: %s" % str(graph_tool.clustering.global_clustering(g.graph)))
 
 
-# Macht einen Ausschnitt um das Wort Aufsichtsrat mit allen direkten Nachbarn. Achtung die Funktion ist noch nicht fertig, es werden nicht die Kookkurrenzen der Nachbarn untereinander übernommen.
+# Macht einen Ausschnitt um das Wort Aufsichtsrat mit allen direkten Nachbarn.
+# Achtung die Funktion ist noch nicht fertig, es werden nicht die Kookkurrenzen
+# der Nachbarn untereinander übernommen.
+
 #sg = g.make_subgraph_around('Aufsichtsrat', 1)
+
 # muss dementsprechend 3 sein.
 #print("pseudo diameter subgraph: %s" % str(graph_tool.topology.pseudo_diameter(sg.graph)))
 g.filter_cooccurrence_threshold(1/15)
@@ -25,4 +34,6 @@ sg.filter_cooccurrence_threshold(1/15)
 print("pseudo diameter subgraph: %s" % str(graph_tool.topology.pseudo_diameter(sg.graph)))
 
 layout = sfdp_layout(sg.graph, C=0.1, eweight=sg.eprop_value_float)
-graph_tool.draw.graph_draw(sg.graph,layout, vertex_text=sg.vprop_word_string, output_size=(1000,1000), output="testout.png", vertex_size=10 )
+graph_tool.draw.graph_draw(sg.graph,layout, vertex_text=sg.vprop_word_string,
+    output_size=(1000,1000), output="testout.png", vertex_size=10,
+    edge_pen_width = 2 )
