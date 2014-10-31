8d74602b945585de891ccb438f0aca6f5b53d6cb
katsel <katsel@users.noreply.github.com>
2014-10-31 18:51:55 +0100

    modified graph appearance; also: code format

--- graph_tool/graph-tool-test.py
+++ graph_tool/graph-tool-test.py
@@ -3,17 +3,16 @@
 from graph_tool.all import *
 from wordsgraph import WordsGraph
 import graph_parser
+import math
 
 # erzeuge aus der Datei test_data_NL einen WordGraph
-# (siehe wordsgraph.py für weitere Dokumentation)
-
+# (siehe wordsgraph.py für weite dokumentation)
 g = graph_parser.file_to_graph('test_data_NL')
 print("graph created")
 
 # graph_tool stellt eine möglichkeit zur Berechnung des Durchmessers eines
 # Graphen zur Verfügung. Die Methode braucht dazu das graph_tool Graph objekt,
 # welches in unserem WordsGraph-Objekt unter dem Feld graph erreichbar ist.
-
 #print("pseudo diameter: %s" % str(graph_tool.topology.pseudo_diameter(g.graph)))
 #print("global clustering: %s" % str(graph_tool.clustering.global_clustering(g.graph)))
 
@@ -21,9 +20,7 @@ print("graph created")
 # Macht einen Ausschnitt um das Wort Aufsichtsrat mit allen direkten Nachbarn.
 # Achtung die Funktion ist noch nicht fertig, es werden nicht die Kookkurrenzen
 # der Nachbarn untereinander übernommen.
-
 #sg = g.make_subgraph_around('Aufsichtsrat', 1)
-
 # muss dementsprechend 3 sein.
 #print("pseudo diameter subgraph: %s" % str(graph_tool.topology.pseudo_diameter(sg.graph)))
 g.filter_cooccurrence_threshold(1/15)
@@ -33,7 +30,10 @@ sg.filter_cooccurrence_threshold(1/15)
 # muss dementsprechend 3 sein.
 print("pseudo diameter subgraph: %s" % str(graph_tool.topology.pseudo_diameter(sg.graph)))
 
+# define layout
 layout = sfdp_layout(sg.graph, C=0.1, eweight=sg.eprop_value_float)
+# draw graph
 graph_tool.draw.graph_draw(sg.graph,layout, vertex_text=sg.vprop_word_string,
-    output_size=(1000,1000), output="testout.png", vertex_size=10,
-    edge_pen_width = 2 )
+    output_size=(1000,1000), output="testout.png",
+    vertex_size=10, edge_pen_width=2, vertex_text_position=7*math.pi/4,
+    vertex_text_color='#2E6A7F', edge_color = sg.eprop_value_float)
