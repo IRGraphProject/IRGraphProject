5c22aed5e7879827a11c25d159a735a65a146b9a
katsel <katsel@users.noreply.github.com>
2015-02-02 20:59:13 +0100

    changed output dirs

--- run_graph_calcs.sh
+++ run_graph_calcs.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir out
-mkdir out/stdouts
-python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
-python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./out/cooc_wiki_en > out/stdouts/cooc_wiki_en
-python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./out/cooc_wiki_sim > out/stdouts/cooc_wiki_sim
+mkdir data/results
+#python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
+python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/cooc_wiki_en > run_wiki_en
+python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/cooc_wiki_sim > run_wiki_sim
