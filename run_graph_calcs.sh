8df8a3aa7cc42d892f9abd7bca1920ce3a9efece
katsel <katsel@users.noreply.github.com>
2015-02-02 21:01:23 +0100

    python3

--- run_graph_calcs.sh
+++ run_graph_calcs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir data/results
-#python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
-python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/cooc_wiki_en > run_wiki_en
-python ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/cooc_wiki_sim > run_wiki_sim
+#python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
+python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/cooc_wiki_en > run_wiki_en
+python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/cooc_wiki_sim > run_wiki_sim
