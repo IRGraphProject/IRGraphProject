8b889551389ba7d58c5c12799129cecd8667522e
katsel <katsel@users.noreply.github.com>
2015-02-03 14:04:49 +0100

    added time output to script

--- run_graph_calcs.sh
+++ run_graph_calcs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-mkdir data/results
+#mkdir data/results
 #python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
-python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/cooc_wiki_en > run_wiki_en
-python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/cooc_wiki_sim > run_wiki_sim
+date
+python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/results/cooc_wiki_en > run_wiki_en
+date
+echo "first one done!"
+python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/results/cooc_wiki_sim > run_wiki_sim
+date
