8fefbe6f65a11ba6049e1f4c58b58942115981fe
katsel <katsel@users.noreply.github.com>
2015-03-04 15:27:46 +0100

    update script

--- run_graph_calcs.sh
+++ run_graph_calcs.sh
@@ -2,8 +2,8 @@
 #mkdir data/results
 #python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_nl.csv ./out/cooc_nl > out/stdouts/cooc_nl
 date
-python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_en.csv ./data/results/cooc_wiki_en > run_wiki_en
+python3 ./graph_tool/cooc_graphcalc.py ./data/cooc_wiki_en.csv ./data/results/cooc_wiki_en > run_wiki_en
 date
 echo "first one done!"
-python3 ./graph_tool/cooc_graphcalc.py -i ./data/cooc_wiki_sim.csv ./data/results/cooc_wiki_sim > run_wiki_sim
+python3 ./graph_tool/cooc_graphcalc.py ./data/cooc_wiki_sim.csv ./data/results/cooc_wiki_sim > run_wiki_sim
 date
