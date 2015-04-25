7f922ef83249b9e63c6ce4bf3a4b2594affb8390
telzul <robert.terbach@googlemail.com>
2015-04-25 16:29:07 +0200

    other calcs

--- graph_tool/random_graph_clustering_coefficient.py
+++ graph_tool/random_graph_clustering_coefficient.py
@@ -8,10 +8,9 @@ def filter_main_component(graph):
     graph.graph.set_vertex_filter(main_component)
     graph.graph.purge_vertices()
     
-filepath = "../data/cooc_wiki_sim.csv"
+filepath = "../data/cooc_denews10k.csv"
 
 graph = graph_parser.file_to_graph(filepath)
-graph.filter_cooccurrence_threshold(1/12)
 filter_main_component(graph)
 
 
@@ -22,3 +21,9 @@ def deg_sampler():
 
 r_graph = random_graph(len(vertex_degrees), deg_sampler, directed=False)
 print(graph_tool.clustering.global_clustering(r_graph))
+
+# cooc_wiki_sim: (0.04836508152618817, 0.0064730643181345245)
+# cooc_wiki_en:  (0.04906310588836009, 0.004693714628953113)
+# cooc_nl:       (0.06654082373918595, 0.0029182368420710997)
+# cooc_denews10k:(0.0690792859801145, 0.006716156468309491)
+
