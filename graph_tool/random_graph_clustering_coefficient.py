737abb3ec1ef8d873830f8b197cbf69506a059d3
telzul <robert.terbach@googlemail.com>
2015-05-01 15:52:40 +0200

    added calcs for small world measure

--- graph_tool/random_graph_clustering_coefficient.py
+++ graph_tool/random_graph_clustering_coefficient.py
@@ -13,10 +13,9 @@ def calc_L(graph):
     counts, bins = graph_tool.stats.distance_histogram(graph,
         float_count= False)
     counts = np.append(counts, 0)
-    print(counts,bins)
-    return 0
-
-filepath = "../data/cooc_denews10k.csv"
+    return sum([ a*b for (a,b) in zip(counts,bins) ]) / (graph.num_vertices() * (graph.num_vertices()-1))
+    
+filepath = "../data/cooc_wiki_sim.csv"
 
 graph = graph_parser.file_to_graph(filepath)
 filter_main_component(graph)
@@ -28,11 +27,28 @@ def deg_sampler():
     return vertex_degrees.pop()
 
 r_graph = random_graph(len(vertex_degrees), deg_sampler, directed=False)
-print("clusterincoefficient")
-print(graph_tool.clustering.global_clustering(r_graph))
+print("clusteringcoefficient")
+print("C_random")
+c_random = graph_tool.clustering.global_clustering(r_graph)
+print(c_random)
+print("C_original")
+c_original = graph_tool.clustering.global_clustering(graph.graph)
+print(c_original)
+print("quotient")
+print(c_original[0]/c_random[0])
+
 print("average shortest path length")
-print(calc_L(r_graph))
-print(calc_L(graph.graph))
+l_random = calc_L(r_graph)
+print("L_random")
+print(l_random)
+l_original = calc_L(graph.graph) 
+print("L_original")
+print(l_original)
+print("quotient")
+print(l_original/l_random)
+print("sigma")
+sigma = (c_original[0] / c_random[0]) / (l_original / l_random)
+print(sigma)
 
 # cooc_wiki_sim: (0.04836508152618817, 0.0064730643181345245)
 # cooc_wiki_en:  (0.04906310588836009, 0.004693714628953113)
