379f0a961a86c4a5f5fa2f239f1d4f28a6ed5c00
katsel <katsel@users.noreply.github.com>
2015-05-04 19:50:39 +0200

    +wordgraphs for longest paths

---
+++ data/results/longpath_wordgraphs/den/create_graphs.sh
@@ -0,0 +1,16 @@
+cd graph_tool
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Aigner -god 3 -w Aigner
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Bommel -god 3 -w Bommel
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/dar -god 3 -w dar
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/davor -god 3 -t 0.075 -w davor
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/EMail -god 4 -w E-Mail
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Erwachsenen -god 3 -t 0.5 -w Erwachsenen
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Financial -god 3 -w Financial
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Guido -god 3 -w Guido
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/HansRudolf -god 4 -w Hans-Rudolf
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Hillary -god 2 -w Hillary
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Kaymer -god 3 -w Kaymer
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Metzelder -god 3 -w Metzelder
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Microsoft -god 3 -w Microsoft
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/Pauli -god 3 -w Pauli
+./cooc_graphcalc.py ../data/cooc_denews10k.csv den/zehnte -god 2 -w zehnte
---
+++ data/results/longpath_wordgraphs/nl/create_graphs.sh
@@ -0,0 +1,9 @@
+cd graph_tool
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/America -god 4 -w America
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/Bowie -god 3 -t 0.12 -w Bowie
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/Dschasira -god 2 -w Dschasira
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/Hoffman -god 5 -w Hoffman
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/Israelis -god 3 -w Israelis
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/OToole -god 5 -t 0.05 -w O’Toole
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/Seymour -god 5 -w Seymour
+./cooc_graphcalc.py ../data/cooc_nl.csv nl/stellt -god 3 -t 0.05 -w stellt
